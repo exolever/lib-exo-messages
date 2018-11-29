2ddd81eac5d2e09dc2e35a16fbcef9a846a20864
Tomas <tomasgarzonhervas@gmail.com>
2018-11-29 10:04:50 +0100

    Bump version: 1.3.1 → 1.3.2

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ EXCLUDE_FROM_PACKAGES = ['tests', ]
 
 setup(
     name='lib-exo-messages',
-    version='1.3.1',
+    version='1.3.2',
     description="""ExO library to manage Messages""",
     long_description=readme + '\n\n' + history,
     author='Javier Sujar',
